b13c5feb3e570b5920544994c75fa367cc3ba50d
anthonypeters <apeters10@luc.edu>
2022-04-02 18:42:33 -0500

    Test Stock Chart

---
+++ main.js
@@ -0,0 +1,60 @@
+Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv', function(err, rows){
+    function unpack(rows, key){
+        return rows.map(function(row) {return row[key];});
+    }
+
+    var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: "AAPL High",
+        x: unpack(rows, 'Date'),
+        y: unpack(rows, 'AAPL.High'),
+        line: {color: '#17BECF'}
+
+    }
+
+    var trace2 = {
+        type: "scatter",
+        mode: "lines",
+        name: "AAPL Low",
+        x: unpack(rows, 'Date'),
+        y: unpack(rows, 'AAPL.Low'),
+        line: {color: '#7F7F7F'}
+
+    }
+
+    var data = [trace1, trace2];
+
+    var layout = {
+        title: 'AAPL Price Time Series',
+        xaxis: {
+            autorange: true,
+            range: ['2015-02-17', '2018-02-17'],
+            rangeselector: {
+                buttons: [
+                    {
+                        count: 1,
+                        label: '1m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    },
+                    {
+                    count: 6,
+                    label: '6m',
+                    stepmode: 'backward'
+                },
+                {step: 'all'}
+                    
+                ]
+        },
+        rangeslider: {range: ['2015-02-17', '2018-02-17']},
+        type: 'date'
+    },
+    yaxis: {
+        autorange: true,
+        range: [86.8700008333, 138.870004167],
+        type: 'linear'
+    }
+};
+    Plotly.newPlot('myDiv', data, layout, {showSendToCloud: true});
+})
