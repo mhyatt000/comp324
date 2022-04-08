7002bb59eb49f3812a0b9db763951fe361c3eca1
anthonypeters <apeters10@luc.edu>
2022-04-08 15:11:41 -0500

    "Grids manual with many tickers"

---
+++ Tickers JS/gs.js
@@ -0,0 +1,14 @@
+new TradingView.widget(
+    {
+    "autosize": true,
+    "symbol": "NYSE:GS",
+    "interval": "D",
+    "timezone": "CDT/UTC",
+    "theme": "dark",
+    "style": "1",
+    "locale": "en",
+    "toolbar_bg": "#f1f3f6",
+    "enable_publishing": false,
+    "container_id": "tradingview_6ec2e"
+  }
+    );
