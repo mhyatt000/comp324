9bfd27a26404e67c92dd3f0867a3b21b6781128e
anthonypeters <apeters10@luc.edu>
2022-04-23 17:40:42 -0500

    Maps working

--- js/maps.js
+++ js/maps.js
@@ -1,10 +1,11 @@
 function initMap() {
     let map = new google.maps.Map(document.getElementById("map"), {
     center: {lat: 41.99910774322552, lng: -87.65829138861581},
-    zoom: 8,
+    zoom: 15,
     });
-}
-
-function build_map(){
-    window.initMap = initMap;
+    new google.maps.Marker({
+        position: {lat: 41.99910774322552, lng: -87.65829138861581},
+        map,
+        title: "Hello World!",
+      });
 }
